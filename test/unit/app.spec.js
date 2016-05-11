c2b2d4abc07468236d178a77c5f49f28ca73e53a
Paulina Sak <paulinasak0@gmail.com>
2016-05-11 13:52:53 +0200

    Paulina Sak

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -11,13 +11,13 @@ describe('app', function () {
         });
         it('should assign person between the ages of 12 - 17 to teens group', function () {
             expect(app.assignToSwimmingCourse('John', '07/10/2001')).toEqual({
-                name: 'John', age: 14, course: 'teens'
+                name: 'Johnny', age: 14, course: 'teens'
             });
         });
-        it('should assign person below 12 to kids group', function () {
+        xit('should assign person below 12 to kids group', function () {
             expect(app.assignToSwimmingCourse('Zack', '10/06/2010')).toEqual({
                 name: 'Zack', age: 5, course: 'kids'
-            });
+            })
         });
     });
 });
