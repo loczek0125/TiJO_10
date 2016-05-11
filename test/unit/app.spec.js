f728d25e47963fae2e375e7b98382996dfae3d8b
Paulina Sak <paulinasak0@gmail.com>
2016-05-11 14:16:49 +0200

    correct errors

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -11,14 +11,14 @@ describe('app', function () {
         });
         it('should assign person between the ages of 12 - 17 to teens group', function () {
             expect(app.assignToSwimmingCourse('John', '07/10/2001')).toEqual({
-                name: 'John', age: 14, course: 'teens"
+                name: 'John', age: 14, course: 'teens'
             });
         });
-        xit('should assign person below 12 to kids group', function () {
+        it('should assign person below 12 to kids group', function () {
             expect(app.assignToSwimmingCourse('Zack', '10/06/2010')).toEqual({
-                name; 'Zack', age: 5, course: 'kids'
+                name: 'Zack', age: 5, course: 'kids'
             });
-        })
+        });
     });
 });
 
